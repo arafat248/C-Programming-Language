3629641eb3bdd054024233bc0d96a887bf09b09d
arafat248 <tanvirarafat6423@gmail.com>
2024-09-21 20:23:55 +0600

    Create Multiply.c

---
+++ Assignment 01/Multiply.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+int main()
+{
+long long int A,B;
+long long int mul;
+scanf("%lld",&A);
+scanf("%lld",&B);
+mul=A*B;
+printf("%lld",mul);
+return 0;
+}
+
+
+
