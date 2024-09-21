a1a73bf6d1070b0b1183047a8b410da5795d8906
arafat248 <tanvirarafat6423@gmail.com>
2024-09-21 20:24:42 +0600

    Create Divisible.c

---
+++ Assignment 01/Divisible.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() 
+{
+long long int N;
+scanf("%lld",&N);
+if(N % 3==0)
+{
+printf("YES");
+}
+else 
+{
+printf("NO");
+}
+    return 0;
+}
