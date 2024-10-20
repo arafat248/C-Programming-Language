8beea00ff35819d80c29df0d8f7704c09522c845
arafat248 <tanvirarafat6423@gmail.com>
2024-10-20 18:56:56 +0600

    Update Tiger vs Pathan.c

--- Final Exam/Tiger vs Pathan.c
+++ Final Exam/Tiger vs Pathan.c
@@ -1,1 +1,40 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+    int t;
+    scanf("%d",&t);
+    while(t--)
+    {
+        int n;
+        scanf("%d",&n);
 
+    char s[100001];
+    scanf("%s",&s);
+    int tiger=0,pathaan=0;
+    for(int i=0;i<n;i++)
+    {
+      if(s[i]=='T')
+    {
+     tiger++;
+    }
+    else if(s[i]=='P')
+    {
+        pathaan++;
+    }
+    }
+    if(tiger>pathaan)
+    {
+        printf("Tiger\n");
+    }
+     else if(tiger<pathaan)
+     {
+         printf("Pathaan\n");
+     }
+     else
+     {
+         printf("Draw\n");
+     }
+    }
+    return 0;
+}
